42f5c121bcbc29ecf7ca55a6c5ec2fc55b90a663
jeonyung1107 <jeonyng@gmail.com>
2017-05-28 20:13:58 +0900

    2353

---
+++ Baek_2352.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Baek_2352 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int [] arr = new int[n];
+		for(int i = 0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		int [] dp = new int[n];
+		for(int i= 0; i<n; i++) dp[i] = 1;
+		for(int i = 0; i<n; i++){
+			for(int j = 0; j<i; j++){
+				if(arr[j] <arr[i]){
+					if(dp[i]<=dp[j]){
+						dp[i] = dp[j] +1;
+					}
+				}
+			}
+		}
+		int m = 0;
+		for(int i = 0; i<n; i++){
+			if(dp[i]>m) m = dp[i];
+		}
+		System.out.println(m);
+	}
+
+}
