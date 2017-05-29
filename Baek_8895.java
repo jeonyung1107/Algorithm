e05e98dfa15016bd45e1e5d3a896651bb7610e75
jeonyung1107 <jeonyng@gmail.com>
2017-05-29 17:24:26 +0900

    finish 8895

--- Baek_8895.java
+++ Baek_8895.java
@@ -4,49 +4,37 @@ public class Baek_8895 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n;
-		int l;
-		int r;
-		int [][][] dp;
+		
+		long [][][] dp;
 		Scanner sc = new Scanner(System.in);
+		int d = sc.nextInt();
+		int n[] = new int[d];
+		int l[] = new int[d];
+		int r[] = new int[d];
 		
-		n = sc.nextInt();
-		l = sc.nextInt();
-		r = sc.nextInt();
-		
-		if(n ==1){
-			System.out.println(1);
-			return;
-		}
-		
-		dp = new int[n][n][n];
+		dp = new long[20+1][20+1][20+1];
 		
 		int i=0;
 		int j=0;
 		int k=0;
 		
-		for(i = 0; i<n;i++){
-			for(j = 0; j<n; j++){
-				for(k = 0; k<n; k++){
-					dp[i][j][k] = 1;
-				}
-			}
-		}
-		i = 0;
-		j = 0;
-		k = 0;
+		dp[1][1][1] = 1;
 		
-		for( i = 0; i<l; i++){
-			if(i!=0)dp[i][j][k] = dp[i-1][j][k];
-			for( j = 0; j<r; j++){
-				if(j!=0)dp[i][j][k] = dp[i][j-1][k];
-				for( k = 1; k<n-i-j; k++){
-					if(i+j>0)dp[i][j][k] = dp[i][j][k-1]*(i+j+k-1);
+		for(i = 2;i<=20; i++){
+			for(j = 1; j<=20; j++){
+				for(k = 1; k<=20; k++){
+					dp[i][j][k] = dp[i-1][j-1][k]+dp[i-1][j][k-1]+dp[i-1][j][k]*(i-2);
 				}
-				k = 0;
 			}
-			j = 0;
 		}
-		System.out.println(dp[l-1][r-1][n-l-r+1]);
+		
+		for(i = 0; i<d; i++){
+			n[i] = sc.nextInt();
+			l[i] = sc.nextInt();
+			r[i] = sc.nextInt();
+		}
+		for(i = 0; i<d; i++){
+			System.out.println(dp[n[i]][l[i]][r[i]]);
+		}
 	}
 }
