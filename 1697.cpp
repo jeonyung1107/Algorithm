f4fdf899dd031256d81566a633540d45ec83824d
Jeon <jeonyng@gmail.com>
2017-10-10 01:00:31 +0900

    1697

--- 1697.cpp
+++ 1697.cpp
@@ -13,9 +13,11 @@ queue<int> q;
 int insert(int now){
 	int tmp[3]={now-1,now+1,now*2};
 	for(int i=0;i<3;i++){
+		/*범위 체크 항상 확실히 해야한다.*/
 		if(is_checked[tmp[i]]!=1&&tmp[i]>=0&&tmp[i]<=100000){
 			q.push(tmp[i]);
 			cost[tmp[i]]=cost[now]+1;
+			/*BFS니까 최초로 q에 들어갈 때가 가장 빨리 방문할 때*/
 			is_checked[tmp[i]]=1;
 		}
 	}
